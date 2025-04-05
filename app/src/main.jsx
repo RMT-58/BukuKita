a648e7c773574e3ff48bc45aabf6c1a9d9caa6aa
dekalouis <dekarebaudo@gmail.com>
2025-04-05 11:37:37 +0700

    feat: finalizing chat feature

--- app/src/main.jsx
+++ app/src/main.jsx
@@ -4,17 +4,12 @@ import "./index.css";
 import App from "./App.jsx";
 import {
   ApolloProvider,
-  ApolloClient,
-  createHttpLink,
-  InMemoryCache,
+  // ApolloClient,
+  // createHttpLink,
+  // InMemoryCache,
 } from "@apollo/client";
-import { setContext } from "@apollo/client/link/context";
+// import { setContext } from "@apollo/client/link/context";
 import { AppProvider } from "./context/AppContext.jsx";
-// import { StrictMode } from "react";
-// import { createRoot } from "react-dom/client";
-// import "./index.css";
-// import App from "./App.jsx";
-// import { ApolloProvider } from "@apollo/client";
 import client from "./config/apollo.js";
 
 createRoot(document.getElementById("root")).render(
