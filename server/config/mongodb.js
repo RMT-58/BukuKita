5dcda72546f331fce044ef51343d55fc02d8b9b8
dekalouis <dekarebaudo@gmail.com>
2025-04-09 09:48:13 +0700

    test: final testing prep

--- server/config/mongodb.js
+++ server/config/mongodb.js
@@ -1,29 +1,31 @@
-import dotenv from "dotenv"
-dotenv.config()
-import { MongoClient } from "mongodb"
+import dotenv from "dotenv";
+dotenv.config();
+import { MongoClient } from "mongodb";
 
-const uri = process.env.MONGODB_URI
-const client = new MongoClient(uri)
-let db = null
+const uri = process.env.MONGODB_URI;
+const client = new MongoClient(uri);
+let db = null;
 
 function connect(isTest = false) {
   try {
-    const dbName = isTest ? `${process.env.MONGODB_DATABASE}_test` : process.env.MONGODB_DATABASE
+    const dbName =
+      process.env.NODE_ENV === "test"
+        ? `${process.env.MONGODB_DATABASE}_test`
+        : process.env.MONGODB_DATABASE;
 
-    db = client.db(dbName)
+    db = client.db(dbName);
   } catch (err) {
-    console.log(err)
+    console.log(err);
   }
 }
 
 export function getDb(isTest = false) {
   if (!db) {
-    connect(isTest)
+    connect(isTest);
   }
-  return db
+  return db;
 }
 
 export function getClient() {
-  return client
+  return client;
 }
-
